ca2f9dfc0407f4114f16767ea5dcac8d90d73966
arikxl <arikxl@gmail.com>
2025-01-21 16:20:47 +0200

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-import './App.css'  
+import './App.css'
 import Room from './components/Room';
 import LoginButton from './components/LoginButton'
 import ChatInterface from './components/ChatInterface'
@@ -12,26 +12,33 @@ const cookies = new Cookies();
 
 function App() {
   const [room, setRoom] = useState(null);
-  const [isUser, setIsUser] = useState(cookies.get('kidkod-user') );
-  
+  const [isUser, setIsUser] = useState(cookies.get('kidkod-user'));
+
 
   return (
-    <>
+    <main>
       {
         isUser
-          ? room 
+          ? room
             ? <ChatInterface />
-            : <Room setRoom={setRoom} />
-          : <LoginButton setIsUser={setIsUser } />
-      }  
-
+            :
+            <div className="center">
+              <Room setRoom={setRoom} setIsUser={setIsUser} />
+            </div>
+          :
+          <div className="center">
+            <h1>KidKod Chat</h1>
+            <LoginButton setIsUser={setIsUser} />
+          </div>
+      }
 
       <LogoutButton setRoom={setRoom} setIsUser={setIsUser} />
 
-      
 
 
-    </>
+
+
+    </main>
   )
 }
 
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,17 +1,25 @@
+import LogoutButton from "./LogoutButton"
 
 
 
 
 
 
-const Room = ({ setRoom }) => {
-    return (
-      
-        <form onSubmit={setRoom}>
-          <p>Enter Room Name</p>
-          <input type="text" />
-          <button type="submit">Let's Go!</button>
-    </form>
+const Room = ({ setRoom, setIsUser }) => {
+  return (
+    <>
+      <form onSubmit={setRoom}>
+        <p>Enter Room Name</p>
+        <input type="text" placeholder="Choose your room..."/>
+        <br />
+        <button type="submit">Let's Go!</button>
+      </form>
+
+      <LogoutButton setRoom={setRoom} setIsUser={setIsUser} />
+    </>
+
+
+
   )
 }
 
