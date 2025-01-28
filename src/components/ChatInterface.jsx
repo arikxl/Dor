9a7e29fbbd112d016a6024aa530982fddb65ba11
Dor <dorjgo@gmail.com>
2025-01-28 21:05:05 +0200

    img and name in chat room

--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -1,13 +1,20 @@
 import {useState} from 'react'
+import { auth } from '../firebaseConfig';
 
 const ChatInterface = () => {
     const [msg, setMsg] = useState("");
+    
     const handleSendMessage = ()=>{
         console.log(msg);
-
+        
     }
+    const user = auth.currentUser;
   return (
     <div>
+        <div>
+          <img src={user?.photoURL}/>
+          <p>hi {user?.displayName}</p>
+        </div>
         <input type="text" onChange={(e)=>setMsg(e.target.value)}></input>
         <button onClick={handleSendMessage}>Send</button>
     </div>
